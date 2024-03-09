3a14026764011aa23d8d20aa9a958689b80466dc
16BrianMrtz5toCP <162483579+BrianMar2024@users.noreply.github.com>
2024-03-08 21:20:17 -0600

    Mavenproject1.java

---
+++ Mavenproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        //Crea una nueva clase llamada Operaciones
+        Operaciones op = new Operaciones();
+        //Muesta en pantalla el resultado de la operacion realizada 
+        System.out.println(op.nose());
+    }
+}
